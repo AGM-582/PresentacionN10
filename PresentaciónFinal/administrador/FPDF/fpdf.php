88531fd975e1a855926b85cf8cae15fe5891d28d
AlanMaggiolo <healer_alan@hotmail.com>
2021-09-11 20:44:23 -0300

    ahora si push del pdf tomando forma 2

--- PresentaciónFinal/administrador/FPDF/fpdf.php
+++ PresentaciónFinal/administrador/FPDF/fpdf.php
@@ -359,19 +359,28 @@ function AddPage($orientation='', $size='', $rotation=0)
 
 function Header()
 {
-	// To be implemented in your own inherited class
+	$this->SetFont('Times','B',9);
+	$this->Line(10,10,206,10);
+	$this->Line(10,35.5,206,35.5);
+	$this->Cell(30,25,'',0,0,'C');
+	$this->Cell(111,25,utf8_decode('ACÁ VA EL TEXTO DE LA INSTITUCIÓN CARPETA FPDF LINEA 366'),0,0,'C', $this->Image('..\Home_page\Normal10.png',10,12,30,20));
+	$this->Ln(5);
+	//Se da un salto de línea de 25
+	$this->Ln(25);
 }
 
 function Footer()
 {
-	// To be implemented in your own inherited class
+	$this->SetY(-15);
+    $this->SetFont('Times','I',8);
+    $this->Cell(0,10,'Este es el pie de página creado con el método Footer() de la clase creada PDF que hereda de FPDF','T',0,'C');
 }
 function ImprimirTexto($file){
 	//Se lee el archivo
 	$txt = file_get_contents($file);
-	$this->SetFont('Arial','',12);
+	$this->SetFont('Times','',12);
 	//Se imprime
-	$this->MultiCell(0,5,$txt);
+	$this->MultiCell(0,5,utf8_decode($txt));
 }
 function PageNo()
 {
